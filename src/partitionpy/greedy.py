b548369110b23186b1f877b8ccc417f040075b1d
Lasse Lueder <lasse.lueder@lueder-mail.de>
2023-05-17 20:56:30 +0200

    fix wrong indices on unsorted input to greedy

--- src/partitionpy/greedy.py
+++ src/partitionpy/greedy.py
@@ -4,14 +4,15 @@ def naive_greedy(numbers: Instance, return_indices=False) -> Partition:
     if len(numbers) < 2:
         raise ValueError("PARTITION instance must contain at least 2 numbers")
 
-    numbers = sorted(numbers, reverse=True)
+    numbers, indices = zip(*sorted(
+            zip(numbers, range(len(numbers))), reverse=True))
 
     list_a = []
     list_b = []
     sum_a = 0
     sum_b = 0
 
-    for idx, number in enumerate(numbers):
+    for idx, number in zip(indices, numbers):
         if sum_a <= sum_b:
             list_a.append(idx if return_indices else number)
             sum_a += number
