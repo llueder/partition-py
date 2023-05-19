85a2783addd43b74955d9c5a2e940e3cef1cea2b
llueder <llueder@users.noreply.github.com>
2023-05-19 16:23:12 +0200

    Ckk (#10)
    * implement graph ops, remove networkx
    
    * add ckk

--- src/partitionpy/__init__.py
+++ src/partitionpy/__init__.py
@@ -2,7 +2,8 @@
 what to say?
 """
 
-__all__ = ["kk", "naive_greedy"]
+__all__ = ["kk", "naive_greedy", "ckk"]
 
 from .kk import karmarkar_karp
 from .naive_greedy import naive_greedy
+from .ckk import complete_karmarkar_karp
