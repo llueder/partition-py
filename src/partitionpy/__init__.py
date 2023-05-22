0ff01c4b9fab9aa337894e225c36d37b0756a611
Lasse Lueder <lasse.lueder@lueder-mail.de>
2023-05-22 16:18:03 +0200

    add kckk

--- src/partitionpy/__init__.py
+++ src/partitionpy/__init__.py
@@ -2,8 +2,9 @@
 what to say?
 """
 
-__all__ = ["kk", "naive_greedy", "ckk"]
+__all__ = ["kk", "naive_greedy", "ckk", "kckk"]
 
 from .kk import karmarkar_karp
 from .naive_greedy import naive_greedy
 from .ckk import complete_karmarkar_karp
+from .kckk import kernelized_complete_karmarkar_karp
